af74fa144efd4d1a56898fb8867208fc470cad08
RafflyAhmad <ahmadraffly91@gmail.com>
2025-12-15 10:26:52 +0700

    Update Register.vue

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -111,7 +111,8 @@ export default {
       success.value = ""
 
       try {
-        const res = await fetch("http://127.0.0.1:8000/register", {
+        const API_URL = import.meta.env.VITE_BACKEND_URL;
+        const res = await fetch(`${API_URL}/register`, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
